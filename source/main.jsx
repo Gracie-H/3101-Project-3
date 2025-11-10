8b7762a5bb6cae8a8a98fc458c193d43dd6c3d2b
Gracie-H <graccee.hong@gmail.com>
2025-11-10 02:20:53 -0500

    st

--- source/main.jsx
+++ source/main.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./App.jsx";
+import "./styles.css";
+createRoot(document.getElementById("root")).render(<App />);
+
 window.addEventListener("load", () => {
   let root = createRoot(document.getElementById("root"));
   root.render(<App />);
